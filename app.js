8655ae85aa2cc17c0b86ef701d3180e5d390fef5
Beatriz-Maria <79063096+Beatriz-Maria@users.noreply.github.com>
2021-05-22 11:14:30 -0300

    Atualização de rotas
    Rotas adicionadas no node para conectar formulario com banco de dados

--- app.js
+++ app.js
@@ -2,14 +2,39 @@
 
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
+const forms = require('/enviar_forms.html')
 
 const PORT = process.env.PORT || 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 const app = express();
 app.use(express.static("public"))
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
+  
 });
 
+// Rota para a página de formulário para responder//
+app.get("/formulario", function(req, res){
+  res.sendFile(path.join(__dirname, "/forms.html"));
+})
+
+//Rota para o botão de enviar do formulário//
+app.post('/enviar_forms.html', function(req, res){
+  res.render('/forms.html')
+} )
+
+//Rota para o botão de enviar conectada com banco de dados
+app.post("/enviar_forms.html", function (req, res){
+  forms.create({//O QUE EU QUERO MOSTRAR//})
+  })
+
+
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on http://localhost:' + PORT)})
---
+++ forms.js
@@ -0,0 +1,10 @@
+// Pasta para conectar com banco de dados ao formulário//
+
+//constante bd=banco de dados//
+
+const bd = require("/bd.js")
+const Forms = bd.sequelize.define('techlabs')
+
+//Exportando o módulo forms//
+
+module.exports = Forms
