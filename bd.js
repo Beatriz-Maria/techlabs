04a796b78f0362f5590f956d38a3dd316d293931
Beatriz-Maria <79063096+Beatriz-Maria@users.noreply.github.com>
2021-05-23 10:10:28 -0300

    Deletando bd.js
    Deletando o arquivo bd.js por motivos de segurança, não expor meu login e senha do banco de dados mySQL

--- bd.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require ('sequelize')
-const sequelize = new Sequelizes ('techlabs', 'root', 'ell5280p#M')
-host:'localhost'
-dialect:"mysql"
-
-
-//Criando um Model para o envio do formulario; o envio se dá pela constante Formulario//
-
